8b26682d80e0829b2329216b7073494ef84e76c4
Majid <mkhan3189@gmail.com>
2018-03-23 23:42:05 +1100

    build

--- samples/jni/build.sh
+++ samples/jni/build.sh
@@ -1,2 +1,3 @@
-g++ -std=c++11 -I$JAVA_HOME/include/ -I$JAVA_HOME/include/darwin/ -I$JAVA_HOME/include/linux/ -lresidue -DELPP_THREAD_SAFE -fPIC -shared -o libresidue-jni.so residue-jni.cpp
-javac ResidueJNIExample.java Residue.java
+g++ -std=c++11 -I$JAVA_HOME/include/ -I$JAVA_HOME/include/darwin/ -I$JAVA_HOME/include/linux/ -lresidue -DELPP_THREAD_SAFE -fPIC -shared -o bin/libresidue-jni.so residue-jni.cpp
+javac -d bin/ ResidueJNIExample.java com/muflihun/residue/Residue.java
+jar cvf bin/Residue.jar bin/com* bin/lib*
